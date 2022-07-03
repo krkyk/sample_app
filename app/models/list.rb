5b5819e250e717d6c36bc7a5e730eb2e06bbe293
krkyk <madeinpink1111@gmail.com>
2022-07-03 06:24:43 +0000

    [Update]詳細ページ

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,17 @@
 class List < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :list_image
 
    with_options presence: true do
     validates :title
     validates :body
   end
+
+  def get_list_image(width, height)
+  unless list_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    list_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    list_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
